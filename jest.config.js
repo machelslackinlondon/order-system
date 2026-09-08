16d16a56a93ddc13d34834f8c0514d54d6ac4a41
Machel Slack <machel.slack@condenast.com>
2026-09-08 19:44:49 +0100

    fix(test): ignore isolated worktrees

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ export default {
   clearMocks: true,
   collectCoverageFrom: ['apps/**/*.js', 'packages/**/*.js', '!**/node_modules/**'],
   coverageDirectory: 'coverage',
+  modulePathIgnorePatterns: ['<rootDir>/.worktrees/'],
   restoreMocks: true,
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.js'],
